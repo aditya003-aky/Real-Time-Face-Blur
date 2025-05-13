2407764b86fe2a5120d7bbae6c84dbcb32ad4852
Aditya Kumar Yadav <120782791+aditya003-aky@users.noreply.github.com>
2025-05-13 17:33:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ while True:
 
         img[y:y + h, x:x + w] = gaussian_blurr
 
-    if faces == ():
+    if len(faces) == ():
         cv2.putText(img, 'No Face Found!', (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
     cv2.imshow('Face Blur', img)
     if cv2.waitKey(1) & 0xff == ord('q'):
